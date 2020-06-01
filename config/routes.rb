018fc2fbdcd7f384a94a9aae3daa30f4cc32402f
megfly <mflynn333@gmail.com>
2020-06-01 16:55:39 -0700

    setup initial files

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  resources :visitors
+  resources :reviews
+  resources :properties
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
