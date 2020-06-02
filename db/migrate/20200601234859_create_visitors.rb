b52f391c56fa248a05c35ed10f2156f30103fb12
megfly <mflynn333@gmail.com>
2020-06-01 18:48:59 -0700

    added attributes to visitors table

--- db/migrate/20200601234859_create_visitors.rb
+++ db/migrate/20200601234859_create_visitors.rb
@@ -1,6 +1,8 @@
 class CreateVisitors < ActiveRecord::Migration[6.0]
   def change
     create_table :visitors do |t|
+      t.string :username
+      t.string :password_digest
 
       t.timestamps
     end
