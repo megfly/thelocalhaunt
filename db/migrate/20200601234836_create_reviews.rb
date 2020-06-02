90853b0a58f70e04ad06afe736ab7fe286f3d3f5
megfly <mflynn333@gmail.com>
2020-06-01 19:18:15 -0700

    added attributes to review table

--- db/migrate/20200601234836_create_reviews.rb
+++ db/migrate/20200601234836_create_reviews.rb
@@ -1,6 +1,14 @@
 class CreateReviews < ActiveRecord::Migration[6.0]
   def change
     create_table :reviews do |t|
+      t.string :title
+      t.text :sighting
+      t.text :noise
+      t.text :smell
+      t.text :cold_spot 
+      t.text :summary
+      t.date :date
+      t.integer :rating
 
       t.timestamps
     end
