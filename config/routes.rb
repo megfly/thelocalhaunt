25de482cf6e1e83189582ab3c93f2bb5141acdeb
megfly <mflynn333@gmail.com>
2020-06-02 17:29:21 -0700

    removed login/lougout routes from property.rb and put them in routes.rb. Added a root route to the application controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
+
+  #rootroute
+  root to: 'application#home'
+
   resources :visitors
   resources :reviews
   resources :properties
+
+  #logging in
+  get '/login', to: 'sessions#new'
+  post '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
