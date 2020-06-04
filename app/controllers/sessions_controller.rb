9e033698f5c5ef683de518b1213369c833efbe50
megfly <mflynn333@gmail.com>
2020-06-03 19:35:43 -0700

    modified the create route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,13 +10,14 @@ class SessionsController < ApplicationController
 
     def create 
         @visitor = Visitor.find_by(username: params[:visitor][:username]) #params coming from loginform. inspect page to see
-        if @visitor && @visitor.authenticate(password: params[:visitor][:password])
+        if params[:username] !="" && params[:password] != ""
+            @visitor.authenticate(params[:password])
             #logging in 
             session[:visitor_id] = @visitor.id 
             redirect_to visitor_path(@visitor) #show page 
         else 
-            render :new 
-            #errors
+            flash[:message] = "Invalid"
+            redirect_to '/login'
         end
     end 
 
--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -8,9 +8,11 @@ class VisitorsController < ApplicationController
         @visitor = Visitor.new(visitor_params)
         if @visitor.save 
             session[:visitor_id] = @visitor.id 
+            flash[:message] = "Your account has been created!"
             redirect_to @visitor #showpage
         else 
-            render :new 
+            flash[:message] = "Please try again"
+            redirect_to '/signup'
         end 
     end 
 
