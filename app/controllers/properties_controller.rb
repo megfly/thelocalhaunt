44c9573ca0e8388231442e7a5369abe40ef2b0d6
megfly <mflynn333@gmail.com>
2020-06-03 20:28:46 -0700

    editing controller because some of the routes arent working

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -2,6 +2,34 @@ class PropertiesController < ApplicationController
     #maybe look at hasmany through in forms lab
 
     def new 
+        @property = Property.new
+    end 
+
+    def create 
+        @property = Property.new(prop_params) #HAAAAAALP
+        if @property.save 
+            redirect_to properties_path
+        else 
+            render :new
+        end 
+    end 
+
+    def index 
+        @properties = Property.all 
+    end 
+
+    def edit 
+        @property = Property.find_by_id(params[:id])
+    end 
+
+    def update 
+        @property = Property.find(params[:id])
+        @property.update(prop_params)
+        redirect_to property_path
+    end 
+
+    def show 
+        @property = Property.find(params[:id])
     end 
 
     private 
