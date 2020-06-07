aac03cd717801bab01ab5349079adf21de621eef
megfly <mflynn333@gmail.com>
2020-06-06 17:45:57 -0700

    modified validations to include the description attribute

--- app/models/property.rb
+++ app/models/property.rb
@@ -2,7 +2,6 @@ class Property < ApplicationRecord
     has_many :reviews #each property may be associated with zero, one or many reviews objects. now we have @property.reviews
     has_many :visitors, through: :reviews #@property.reviews
 
-    validates :name, presence: true 
-    validates :location, presence: true 
+    validates :name, :location, :description, presence: true 
 
 end
