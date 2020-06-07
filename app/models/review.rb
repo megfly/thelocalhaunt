370510f2ec04bc0ff04287113ead9187d89844f2
megfly <mflynn333@gmail.com>
2020-06-06 17:47:01 -0700

    added scopes and added more attributes to validations

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,8 +3,12 @@ class Review < ApplicationRecord
     belongs_to :visitor #each review associated with one visitor, now we have @review.visitor
     belongs_to :property #each review associated with on property, now we have @review.property
 
-    validates :title, presence: true 
+    validates :title, :sighting, :noise, :smell, :cold_spot, :summary, :date, presence: true 
 
-    scope :ordered_rating, -> { order(rating: 'desc') }
-
+    scope :ordered_rating, -> { order(rating: :desc) }
+    scope :most_recent_review, -> { order(date: :desc) }
+    #ex scope :long_titles, -> { where("LENGTH(title) > 30") } gives speciufic query a name, go back to controller  
+    #controller def index @books = Book.long_ttiles end 
+    #go to console and do Book.long_titles
+    
 end
