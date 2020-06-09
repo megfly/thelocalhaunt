e75404fa38b09c185af62d45196b8d5bb9df492e
megfly <mflynn333@gmail.com>
2020-06-09 11:00:27 -0700

    added email to visitors table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_01_234859) do
+ActiveRecord::Schema.define(version: 2020_06_07_042720) do
 
   create_table "properties", force: :cascade do |t|
     t.string "name"
@@ -40,6 +40,7 @@ ActiveRecord::Schema.define(version: 2020_06_01_234859) do
     t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
   end
 
 end
