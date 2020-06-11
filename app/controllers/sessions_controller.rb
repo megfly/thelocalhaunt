efdebb88630793a7bc7553c0dc76aa77b5219113
megfly <mflynn333@gmail.com>
2020-06-10 22:15:04 -0700

    modified the destroy route and the create route

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,18 @@
 class SessionsController < ApplicationController
     #logging in and logging out
 
+    def destroy 
+        session.clear
+        redirect_to root_path
+    end 
+
     def home 
     end 
 
     def create 
-        @visitor = Visitor.find_by(username: params[:session][:username]) #params coming from loginform.
-      
-        if @visitor && @visitor.authenticate(params[:session][:password])
+        @visitor = Visitor.find_by(username: params[:visitor][:username]) #params coming from loginform.
+
+        if @visitor && @visitor.authenticate(params[:visitor][:password])
              session[:visitor_id] = @visitor.id  #save the user id inside the browser cookie and log in
              redirect_to visitor_path(@visitor) #show page 
         else 
@@ -24,10 +29,6 @@ class SessionsController < ApplicationController
         redirect_to visitor_path(@visitor)
       end
 
-    def destroy 
-        session.clear
-        redirect_to root_path 
-    end 
 
     private 
 
